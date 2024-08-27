883a20fb81502521f965f4d98f30e4a3cd017e8d
Слава <vyacheslavdobr@gmail.com>
2024-08-27 14:32:31 +0300

    lesson_8 added

--- lesson_8/team.py
+++ lesson_8/team.py
@@ -1,30 +1,18 @@
-TEAM_TYPE = dict[int, dict]
+from . import database
 
-team: TEAM_TYPE = {
-    1: {"name":"John", "age": 20}, 
-    3: {"name":"Mark", "age": 33},
-    31: {"name":"Kevin", "age": 31}
-}
+def repr_players(): 
+    team: dict[int,dict] = database.get_team()
+    for number, player in team.items():
+        print(f"\t[Player {number}]: {player['name']}, {player["age"]}")
 
-def repr_players(players: TEAM_TYPE):
-    for player in players:
-        print(
-            f"\t[Player {player['number']}]: {player['name']}, {player["age"]}"
-            )
-
-def player_add(name: str, age: int, number: int) -> dict:
-    for player in team:
-        if player["number"] == number:
-            print(f"the player with #{number} exists")
-            break
-        else:
-            player: dict = {
-                "name" : name,
-                "age" : age,
-                "number" : number, 
-            }
-            team.append(player)
-            return player
+def player_add(name: str, age: int, number: int) -> dict | None:
+    player: dict = {"name":name, "age":age}
+    saved: bool = database.save(id_=number, instance=player) #can add "debug=True" 
+    
+    if not saved:
+        print(f"player# {number} already exists")
+    else:
+        return player
         
 def player_update(new_name: str, new_age: int, player_num:int)->None:
     is_found = False
@@ -40,10 +28,14 @@ def player_update(new_name: str, new_age: int, player_num:int)->None:
         
             
 
-def player_delete(number: int)-> None:
-    for player in team:
-        if player["number"] == number:
-            del player
+def player_delete(number: int)-> bool:
+    team: dict[int, dict] = database.get_team()
+    if not team.get(number):
+        return False
+    else:
+        database.delete(id_=number)
+        return True
+
 
 def main():    
     operations: tuple[str, ...] = ("add", "del", "repr", "upd", "exit")
@@ -53,12 +45,14 @@ def main():
         if operation not in operations:
             print(f"Operation {operation}, is not available\n")
             continue
-        
+            
         if operation == "exit":
             print("Bye")
             break
+        
         elif operation == "repr":
-            repr_players(team)
+            repr_players()
+            
         elif operation == "add":
             user_data = input("Enter the new player information [name, age, number]: ")
             user_items: list[str] = user_data.split(",")
@@ -68,6 +62,7 @@ def main():
             except ValueError:
                 print("Age and number must be integers\n\n")
                 continue
+            
         elif operation == "upd":
             user_data = input("Enter a new name, age and# [name, age, number]: ")
             user_items: list[str] = user_data.split(",")
@@ -78,6 +73,16 @@ def main():
                 ValueError
                 print(f"there is no player with # {num}")
                 continue
+        
+        elif operation == "del":
+            user_data = input("Enter the palyer's # [int]: ")
+            try:
+                _user_data = int(user_data)
+            except ValueError:
+                print("player's# must be an integer")
+                continue
+            else:
+                player_delete(number=_user_data)
 
         else:
             raise NotImplementedError
